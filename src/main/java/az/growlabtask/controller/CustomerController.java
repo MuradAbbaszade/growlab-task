1b8c3ce10433df23f2e4c7792f71dd5794839ac4
Samira08 <samireatluhanova@gmail.com>
2023-12-01 15:59:15 +0400

    last change

--- src/main/java/az/growlabtask/controller/CustomerController.java
+++ src/main/java/az/growlabtask/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package az.growlabtask.controller;
 
-import az.growlabtask.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,17 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
-    private final UserRepository userRepository;
-
-    @GetMapping("/a")
-    public String a(){
-        /*Set<Role> roleSet = user.getRoleSet();
-        for (Role role:roleSet){
-            System.out.println(role.getAttributeSet());
-        }*/
-        return "customer";
-    }
-
     @GetMapping
     public String customer(){
         System.out.println("it works");
